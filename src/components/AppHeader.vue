74839d2aed32c0836406a745c07982218675e1d2
Seme87 <massimomonteleone@hotmail.com>
2022-11-24 16:44:23 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<script>
+
+
+export default {
+  name: 'AppHeader',
+  
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1>Header</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+
+
+// header {
+//     text-align: center;
+//     .main-nav {
+//         ul {
+//             display: flex;
+//             list-style: none;
+//             justify-content: center;
+
+//             a {
+//                 display: inline-block;
+//                 padding: 15px 25px;
+//                 text-decoration: none;
+//                 color: #000;
+
+//                 &.active {
+//                     background-color: var(--secondary-color);
+//                 }
+//             }
+//         }
+//     }
+// }
+</style>
